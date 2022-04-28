3a1d58145e9a50a7ded0259651d981a0307e2ebe
Mukul1212 <102962459+Mukul1212@users.noreply.github.com>
2022-04-28 17:04:10 +0530

    changedone

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 
-
+// take express in app
 const app = express();
 
 app.set('view engine', 'ejs');
